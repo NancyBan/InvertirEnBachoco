de0991514c811dc73b977f851a3dc27fba64472e
NancyBan <108419686+NancyBan@users.noreply.github.com>
2022-07-11 14:56:03 -0500

    Uw

---
+++ js/js.js
@@ -0,0 +1,16 @@
+//se crea la variable que guarda la seleecion del formulario
+var select = document.querySelector("#selectGOD")
+
+//Se crea la función que cambiará la selección que hagas
+function cambioBienPro(){
+	//Si la opcion que elegiste no es la 4, te dirá lo siguiente
+	if (select.value!=4){
+		alert("Estas invirtiendo muy poco!!! Quieres retirarte a los 21 años o no?");
+	}
+	//Si tu seleccion es la 4, te dirá lo siguiente
+	if(select.value==4){
+		alert("Si le sabes wey, te vas a hacer millonario");
+	}
+}
+//Se crea el evento click
+select.addEventListener("click", cambioBienPro);
